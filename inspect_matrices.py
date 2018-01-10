5e6ccc92b1f27437cc4b93d46f149a8f595f25c8
Ian Hunter <ian.hunter@intel.com>
2018-01-10 09:01:06 +0000

    taking the delta operation outside the function so that it can be modified in future with ease, or single matrix thresholding can be done

--- inspect_matrices.py
+++ inspect_matrices.py
@@ -8,22 +8,22 @@ class ErrorType(Enum):
     PARTIAL_ERROR = auto()
     NO_ERROR = auto()
 
-def check_dimension(result, expected, dimension_labels):
+def check_dimension(delta, dimension_labels):
     wrong = 0
     leaf_errors = []
-    if len(result.shape) > 1:
-        for i in range(len(result)):
-            err = check_dimension(result[i], expected[i],dimension_labels[1:])
+    if len(delta.shape) > 1:
+        for i in range(len(delta)):
+            err = check_dimension(delta[i],dimension_labels[1:])
             if err == ErrorType.COMPLETE_ERROR:
                 leaf_errors.append(i)
                 wrong += 1   
     else:
-        for i in range(len(result)):
+        for i in range(len(delta)):
             global threshold
-            if np.abs(result[i] - expected[i]) > threshold:
+            if delta[i] > threshold:
                 leaf_errors.append(i)
                 wrong += 1
-    if wrong == len(result):
+    if wrong == len(delta):
         return ErrorType.COMPLETE_ERROR
     elif wrong > 0:
         print("The dimension", dimension_labels[0], "has errors for elements:", leaf_errors)
@@ -57,10 +57,12 @@ if (result.shape != expected.shape):
     print("Error: Shape of Matrices must match")
     quit(-1)
 
+delta = np.abs(result - expected)
+
 dimensions = len(expected.shape)
 dimension_labels = list(string.ascii_uppercase[-dimensions:])
 
 print("Overall Number of Dimensions:",dimensions, dimension_labels,"\n")
 
-check_dimension(result, expected, dimension_labels)
+check_dimension(delta, dimension_labels)
 
